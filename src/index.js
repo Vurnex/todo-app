e445e3a11c2686b2899fc521923870cd1fc1d869
Vurnex <hamilton.laquon@gmail.com>
2024-06-15 08:56:02 -0500

    Add logic for grabbing button text

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import _ from 'lodash'
 import './style.css';
 
+let proj_header = document.getElementById('projName');
+
 function component() {
 
     const element = document.createElement('div');
-
-    // Lodash, currently included via a script, is required for this line to work
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
     return element;
   
 }
 
 document.body.appendChild(component());
+
+document.getElementById('inboxBtn').addEventListener('click', () => {
+
+    proj_header.textContent = "Inbox";
+
+});
+
+document.getElementById('todayBtn').addEventListener('click', () => {
+
+    proj_header.textContent = "Today";
+
+});
+
+document.getElementById('thisWeekBtn').addEventListener('click', () => {
+
+    proj_header.textContent = "This Week";
+
+});
+
+document.getElementById('projBtn').addEventListener('click', () => {
+
+    let projectTitle = document.getElementsByClassName('project-btn-name')[0];
+
+    proj_header.textContent = projectTitle.textContent;
+
+});
