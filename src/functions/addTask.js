31250bdc0a19fbed1290d1a8cd7ed61f746b744a
Vurnex <hamilton.laquon@gmail.com>
2024-08-05 09:12:19 -0500

    Logic for task button

--- src/functions/addTask.js
+++ src/functions/addTask.js
@@ -3,4 +3,42 @@ import newTask from "./tasks";
 export default function addTask(taskDesc) {
 
     let taskDescription = newTask(taskDesc);
+
+    let newButton = document.createElement('button');
+
+    newButton.classList.add("button-task", "btn", "btn-light");
+
+    let leftElements = document.createElement('div');
+    leftElements.classList.add("left-task-panel", "form-check");
+
+    let leftInput = document.createElement('input');
+    leftInput.classList.add("form-check-input");
+    leftInput.setAttribute("type", "checkbox");
+
+    let leftDesc = document.createElement('p');
+    leftDesc.classList.add("task-content");
+    leftDesc.textContent = "New Dynamic Task";
+
+    leftElements.appendChild(leftInput);
+    leftElements.appendChild(leftDesc);
+
+    newButton.appendChild(leftElements);
+
+    let rightElements = document.createElement('div');
+    rightElements.classList.add("right-task-panel");
+
+    let rightInput = document.createElement('input');
+    rightInput.setAttribute("type", "date");
+    rightInput.setAttribute("name", "");
+    rightInput.setAttribute("id", "");
+
+    let rightDesc = document.createElement('p');
+    rightDesc.classList.add("task-date");
+
+    rightElements.appendChild(rightDesc);
+    rightElements.appendChild(rightInput);
+
+    newButton.appendChild(rightElements);
+
+    return newButton;
 }
